5f7021904723b5204d52b4bc219806517469332e
Jordan Ell <jordan.ell7@gmail.com>
2014-08-12 21:09:07 -0700

    Initial commit to the android humans app.

---
+++ app/src/main/java/com/humansapp/humans/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.humansapp.humans;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+/**
+ * Created by jordan on 2014-08-12.
+ */
+public class SplashScreen extends Activity {
+
+    // Timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MyActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
