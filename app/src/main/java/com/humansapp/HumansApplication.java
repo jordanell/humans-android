112b723d57e7ff62bf3ba5278bda7e36c36d1b66
Jordan Ell <jordan.ell7@gmail.com>
2014-08-14 22:09:42 -0700

    Moved debugging code to application.

--- app/src/main/java/com/humansapp/HumansApplication.java
+++ app/src/main/java/com/humansapp/HumansApplication.java
@@ -2,11 +2,23 @@ package com.humansapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.x500.X500Principal;
 
 /**
  * Created by jordan on 2014-08-14.
  */
 public class HumansApplication extends Application {
+    private static final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
+
     private static Context context;
 
     public void onCreate(){
@@ -16,5 +28,37 @@ public class HumansApplication extends Application {
 
     public static Context getAppContext() {
         return HumansApplication.context;
+    }
+
+    public static boolean isDebuggable()
+    {
+        Context ctx = getAppContext();
+        boolean debuggable = false;
+
+        try
+        {
+            PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
+            Signature signatures[] = pinfo.signatures;
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            for ( int i = 0; i < signatures.length;i++)
+            {
+                ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
+                X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);
+                debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
+                if (debuggable)
+                    break;
+            }
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            //debuggable variable will remain false
+        }
+        catch (CertificateException e)
+        {
+            //debuggable variable will remain false
+        }
+        return debuggable;
     }
 }
--- app/src/main/java/com/humansapp/humans/rest/HumansRestClient.java
+++ app/src/main/java/com/humansapp/humans/rest/HumansRestClient.java
@@ -20,8 +20,6 @@ import java.security.cert.X509Certificate;
 import javax.security.auth.x500.X500Principal;
 
 public class HumansRestClient {
-    private final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
-
     private static HumansRestClient instance = null;
 
     private AsyncHttpClient client;
@@ -30,7 +28,7 @@ public class HumansRestClient {
     protected HumansRestClient() {
         this.client = new AsyncHttpClient();
 
-        if(isDebuggable(HumansApplication.getAppContext())) {
+        if(HumansApplication.isDebuggable()) {
             this.baseURL = "http://localhost:4444/";
         } else {
             this.baseURL = "http://api.humansapp.com:4444/";
@@ -55,36 +53,5 @@ public class HumansRestClient {
 
     private String getAbsoluteUrl(String relativeUrl) {
         return baseURL + relativeUrl;
-    }
-
-    private boolean isDebuggable(Context ctx)
-    {
-        boolean debuggable = false;
-
-        try
-        {
-            PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
-            Signature signatures[] = pinfo.signatures;
-
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-
-            for ( int i = 0; i < signatures.length;i++)
-            {
-                ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
-                X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);
-                debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
-                if (debuggable)
-                    break;
-            }
-        }
-        catch (PackageManager.NameNotFoundException e)
-        {
-            //debuggable variable will remain false
-        }
-        catch (CertificateException e)
-        {
-            //debuggable variable will remain false
-        }
-        return debuggable;
     }
 }
