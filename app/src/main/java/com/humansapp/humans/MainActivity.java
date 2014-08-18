4ed3272d72e3eb82ff1fad6e2079910caae15e6e
Jordan Ell <jordan.ell7@gmail.com>
2014-08-17 20:39:08 -0700

    Added the manual refresh button.

--- app/src/main/java/com/humansapp/humans/MainActivity.java
+++ app/src/main/java/com/humansapp/humans/MainActivity.java
@@ -82,6 +82,9 @@ public class MainActivity extends ListActivity {
             }
         }
 
+        // Clear old list
+        setListAdapter(null);
+
         RequestParams params = new RequestParams();
         params.put("user_id", userId);
 
@@ -179,9 +182,17 @@ public class MainActivity extends ListActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch(id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_refresh:
+                mPullToRefreshLayout.setRefreshing(true);
+                loadConversations();
+                return true;
+
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
