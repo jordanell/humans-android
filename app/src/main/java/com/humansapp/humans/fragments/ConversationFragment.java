ddaabab1342aa0ac27f6144db383f6bea104c57b
Jordan Ell <jordan.ell7@gmail.com>
2014-08-19 22:31:23 -0700

    Started the message loading skeleton.

--- app/src/main/java/com/humansapp/humans/fragments/ConversationFragment.java
+++ app/src/main/java/com/humansapp/humans/fragments/ConversationFragment.java
@@ -8,8 +8,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.humansapp.humans.R;
+import com.humansapp.humans.rest.HumansRestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
 
 /**
  * Created by jordan on 2014-08-18.
@@ -19,6 +29,10 @@ public class ConversationFragment extends Fragment {
     private String name;
 
     private View view;
+
+    private RelativeLayout loading;
+    private ListView content;
+    private TextView empty;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,7 +60,58 @@ public class ConversationFragment extends Fragment {
             getActivity().getActionBar().setTitle(name);
         }
 
+        // Get view components
+        this.loading = (RelativeLayout) view.findViewById(R.id.loading);
+        this.content = (ListView) view.findViewById(R.id.content);
+        this.empty = (TextView) view.findViewById(R.id.empty);
+
+        loadMessages();
+
         return view;
+    }
+
+    private void loadMessages() {
+        // Show we are loading something
+        loading.setVisibility(View.VISIBLE);
+        content.setVisibility(View.GONE);
+        empty.setVisibility(View.GONE);
+
+        RequestParams params = new RequestParams();
+        params.put("user_id", HumansRestClient.instance().getUserId());
+        params.put("conversation_id", this.conversationId);
+
+        HumansRestClient.instance().get("messages", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject response) {
+                showError();
+            }
+        });
+    }
+
+    private void showError() {
+        LinearLayout error = (LinearLayout) view.findViewById(R.id.error_layout);
+        error.setVisibility(View.VISIBLE);
+
+        error.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retry();
+            }
+        });
+    }
+
+    private void retry() {
+        LinearLayout error = (LinearLayout) view.findViewById(R.id.error_layout);
+        error.setVisibility(View.GONE);
+
+        error.setOnClickListener(null);
+
+        loadMessages();
     }
 
     @Override
